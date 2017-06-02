b6e661492471266a2793722c25708e667530a085
rstar2 <rstar2@abv.bg>
2017-06-02 11:33:04 +0300

    add logging

--- util_logging.py
+++ util_logging.py
@@ -7,6 +7,14 @@ import logging
 # New filters can select different routing based on
 # message priority: DEBUG, INFO, WARNING, ERROR, and CRITICAL.
 
+
+# I. Configure the logging - basic configuration
+#  filemode='w' will replace the existing 'example.log'on each start
+logging.basicConfig(filename='example.log',
+                    filemode='w', level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)s: %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+
+# II. using the ROOT logger
 logging.debug('Debugging information')
 logging.info('Informational message')
 logging.warning('Warning:config file %s not found', 'server.conf')
@@ -16,3 +24,13 @@ logging.critical('Critical error -- shutting down')
 # The logging system can be configured directly from Python
 # or can be loaded from a user editable configuration file
 # for customized logging without altering the application
+
+# ---------------
+# III. Using different loggers
+
+log = logging.getLogger("foo.bar.baz")
+
+#  so it good to create loggers with module's name in the Python package namespace.
+log = logging.getLogger(__name__)
+
+log.warning('%s before you %s', 'Look', 'leap!')
---
+++ util_logging_ex.py
@@ -0,0 +1,70 @@
+import logging
+
+# IV. Advanced configration
+
+# Programmers can configure logging in three ways:
+# 1. Creating loggers, handlers, and formatters explicitly using Python code that calls the configuration methods listed above.
+# 2. Creating a logging config file and reading it using the fileConfig() function.
+# 3. Creating a dictionary of configuration information and passing it to the dictConfig() function.
+
+
+# 1 - Programatically
+# create logger
+logger = logging.getLogger('simple_example')
+logger.setLevel(logging.DEBUG)
+
+# create console handler and set level to debug
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+# create formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# add formatter to ch
+ch.setFormatter(formatter)
+
+# add ch to logger
+logger.addHandler(ch)
+
+# 'application' code
+logger.debug('debug message')
+logger.info('info message')
+logger.warning('warn message')
+logger.error('error message')
+logger.critical('critical message')
+
+# 2. Using a confguration file
+
+import logging.config
+
+# create configuration
+logging.config.fileConfig('logging.properties')
+
+# create logger
+logger = logging.getLogger('simpleExamplePROPS')
+
+# 'application' code
+logger.debug('debug message')
+logger.info('info message')
+logger.warning('warn message')
+logger.error('error message')
+logger.critical('critical message')
+
+
+# 3. Usiging dictionary - it can come from file/socket/creaed_here/...
+
+import yaml
+
+# create configuration
+logging.config.dictConfig(yaml.load(open('logging.yaml', 'r')))
+
+# # create logger
+logger = logging.getLogger('simpleExampleYAML')
+
+# 'application' code
+logger.debug('debug message')
+logger.info('info message')
+logger.warning('warn message')
+logger.error('error message')
+logger.critical('critical message')
+
