01aa1f9e5f560e1de866445d8679100a9b3c6985
rstar2 <rstar2@abv.bg>
2017-07-31 12:46:16 +0300

    added stripe

--- flask_spa/app/routes/shop.py
+++ flask_spa/app/routes/shop.py
@@ -1,15 +1,43 @@
+import os
 import time
 import requests
 import requests_cache
 
 from flask import render_template, request, jsonify
 
+import stripe
+
 from . import routes
 
-# set a SQLite cache for the requests
-requests_cache.install_cache('github_cache', backend='sqlite', expire_after=180)
+stripe_keys = {
+    'secret_key': os.environ['SECRET_KEY'],
+    'publishable_key': os.environ['PUBLISHABLE_KEY']
+}
+
+# initialize stripe
+stripe.api_key = stripe_keys['secret_key']
 
 
 @routes.route('/shop/<int:product_id>', methods=['GET'])
 def shop(product_id):
-    return render_template('shop.html', product_id=product_id)
+    return render_template('shop.html', product_id=product_id, stripe_key=stripe_keys['publishable_key'])
+
+
+@routes.route('/buy', methods=['POST'])
+def buy(product_id):
+    # Amount in cents
+    amount = 500
+
+    customer = stripe.Customer.create(
+        email='customer@example.com',
+        source=request.form['stripeToken']
+    )
+
+    charge = stripe.Charge.create(
+        customer=customer.id,
+        amount=amount,
+        currency='usd',
+        description='Flask Charge'
+    )
+
+    return render_template('thanks.html', product_id=product_id, amount=amount)
